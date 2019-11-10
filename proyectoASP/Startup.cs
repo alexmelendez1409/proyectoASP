f16d19bd4a94a206bed838b0aa76c722d5bbef42
Alex Melendez <caml1409@gmail.com>
2019-11-09 21:18:40 -0600

    Agregar archivos de proyecto.

---
+++ proyectoASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(proyectoASP.Startup))]
+namespace proyectoASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
